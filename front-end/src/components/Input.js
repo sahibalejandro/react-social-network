11d01d5f3d8e10f80a0189dacac69b5edbb4503e
Sahib <sahib.alejandro@gmail.com>
2018-02-09 22:58:19 -0600

    Simplify how the class names are created in the Input component

--- front-end/src/components/Input.js
+++ front-end/src/components/Input.js
@@ -7,18 +7,8 @@ class Input extends React.Component {
         errors: PropTypes.object.isRequired,
     }
 
-    classNames = () => {
-        const classNames = ['form-control']
-
-        if (this.props.errors[this.props.name]) {
-            classNames.push('is-invalid')
-        }
-
-        return classNames.join(' ')
-    }
-
     render() {
-        return <input className={this.classNames()} {...this.props} />
+        return <input className={`form-control ${this.props.errors[this.props.name] ? 'is-invalid' : null}`} {...this.props} />
     }
 }
 
